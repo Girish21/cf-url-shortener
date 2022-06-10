4f4c61d12c0d271dd05b16d9aaeca16a6549ff68
Girish21 <girishkumarv21@gmail.com>
2022-06-11 00:03:23 +0530

    🛠 fix focus ring

--- packages/remix-app/app/components/form.tsx
+++ packages/remix-app/app/components/form.tsx
@@ -23,7 +23,7 @@ function Form() {
             placeholder='URL...'
             className='rounded-md px-2 py-3 sm:px-3 sm:py-4 flex-1 shadow-[4px_4px] border-2 border-gray-900 dark:border-none shadow-gray-900 dark:shadow-none focus:outline-none dark:focus-visible:ring-2 dark:ring-blue-500 dark:ring-offset-2 dark:ring-offset-transparent text-lg sm:text-xl transition-shadow duration-150 ease-out'
           />
-          <button className='bg-violet-600 text-white font-bold rounded-md px-2 py-3 sm:px-3 sm:py-4 focus:outline-none dark:focus-visible:ring-4 dark:ring-offset-2 dark:ring-offset-transparent dark:ring-blue-500 text-lg sm:text-xl shadow-[5px_5px] focus-visible:shadow-[7px_7px] transition-shadow duration-150 ease-out shadow-gray-900 focus-visible:shadow-gray-900 border-2 border-gray-900 dark:border-none'>
+          <button className='bg-violet-600 text-white font-bold rounded-md px-2 py-3 sm:px-3 sm:py-4 focus:outline-none dark:focus-visible:ring-2 dark:ring-offset-2 dark:ring-offset-transparent dark:ring-blue-500 text-lg sm:text-xl shadow-[5px_5px] focus-visible:shadow-[7px_7px] transition-shadow duration-150 ease-out shadow-gray-900 focus-visible:shadow-gray-900 border-2 border-gray-900 dark:border-none'>
             Get URL
           </button>
         </fieldset>
