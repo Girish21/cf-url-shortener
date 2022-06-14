7d8b9ccdbd4ce83571c9ef293aef414517066cc4
Girish21 <girishkumarv21@gmail.com>
2022-06-15 00:11:58 +0530

    fix a minor tailwind bug

--- packages/remix-app/app/components/container.tsx
+++ packages/remix-app/app/components/container.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 function Container({ children }: { children: React.ReactNode }) {
-  return <main className='mx-auto w-[min(60ch,100vw-2rem)]'>{children}</main>
+  return <main className='mx-auto w-[min(60ch,100vw_-_2rem)]'>{children}</main>
 }
 
 export default Container
