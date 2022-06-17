3535d913707b40d218a6ee0685a667441f756559
Girish21 <girishkumarv21@gmail.com>
2022-06-17 23:41:04 +0530

    form css correction and minor ux improvement to input field

--- packages/remix-app/app/components/form.tsx
+++ packages/remix-app/app/components/form.tsx
@@ -1,15 +1,22 @@
-import { Form as RemixForm } from '@remix-run/react'
+import { Form as RemixForm, useTransition } from '@remix-run/react'
 import * as React from 'react'
 
 function Form() {
-  const inputRef = React.useRef<HTMLInputElement>(null)
+  let transition = useTransition()
+  let inputRef = React.useRef<HTMLInputElement>(null)
 
   React.useEffect(() => {
     inputRef.current?.focus()
   }, [])
 
+  React.useEffect(() => {
+    if (transition.state === 'idle' && inputRef.current) {
+      inputRef.current.value = ''
+    }
+  }, [transition.state])
+
   return (
-    <div className='pt-[10vh]'>
+    <div className='mt-[10vh]'>
       <RemixForm method='post'>
         <fieldset className='mx-auto flex flex-col gap-4 sm:flex-row'>
           <label htmlFor='url' className='sr-only'>
@@ -21,9 +28,9 @@ function Form() {
             name='url'
             required
             placeholder='URL...'
-            className='flex-1 rounded-md border-2 border-gray-900 px-2 py-3 text-lg shadow-[4px_4px] shadow-gray-900 transition-shadow duration-150 ease-out focus:outline-none dark:border-none dark:shadow-none dark:ring-blue-500 dark:ring-offset-2 dark:ring-offset-transparent dark:focus-visible:ring-2 sm:px-3 sm:py-4 sm:text-xl'
+            className='flex-1 rounded-md border-2 border-gray-900 px-2 py-3 text-lg shadow-[4px_4px] shadow-gray-900 transition-shadow duration-150 ease-out focus:outline-none focus-visible:shadow-[6px_6px] focus-visible:shadow-gray-900 dark:border-none dark:shadow-none dark:ring-blue-500 dark:ring-offset-2 dark:ring-offset-transparent dark:focus-visible:ring-2 sm:px-3 sm:py-4 sm:text-xl'
           />
-          <button className='rounded-md border-2 border-gray-900 bg-violet-600 px-2 py-3 text-lg font-bold text-white shadow-[5px_5px] shadow-gray-900 transition-shadow duration-150 ease-out focus:outline-none focus-visible:shadow-[7px_7px] focus-visible:shadow-gray-900 dark:border-none dark:ring-blue-500 dark:ring-offset-2 dark:ring-offset-transparent dark:focus-visible:ring-2 sm:px-3 sm:py-4 sm:text-xl'>
+          <button className='rounded-md border-2 border-gray-900 bg-violet-600 px-2 py-3 text-lg font-bold text-white shadow-[4px_4px] shadow-gray-900 transition-shadow duration-150 ease-out focus:outline-none focus-visible:shadow-[6px_6px] focus-visible:shadow-gray-900 dark:border-none dark:ring-blue-500 dark:ring-offset-2 dark:ring-offset-transparent dark:focus-visible:ring-2 sm:px-3 sm:py-4 sm:text-xl'>
             Get URL
           </button>
         </fieldset>
